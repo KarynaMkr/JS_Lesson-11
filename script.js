075a32c5fa07c25bd6bd9c881378753ef4af8f55
KarynaMkr <mkrtchian.karyna@gmail.com>
2022-10-04 21:21:13 +0300

    isBetween done

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+  // -------- IS BETWEEN --------
+
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+function getRangeNumber (message) {
+  let number;
+  do {
+    number = prompt(message);
+    if (isNaN(number)) {
+      alert('Entered is NOT A NUMBER, repeat please');
+    } else if (number == '') {
+      alert('Your string is empty. Enter the number');
+    }
+  } while (isNaN(number) || number == '');
+  return number;
+};
+
+
 function isBetween(min, max) {
-  min = prompt('Please enter min item from array: ' + array);
-  max = prompt('Please enter max item from array: ' + array);
+  min = getRangeNumber('Please enter MIN item from array: [' + array + ']');
+  max = getRangeNumber('Please enter MAX item from array: [' + array + ']');
   return function(item) {
     return item >= min && item <= max;
   }
